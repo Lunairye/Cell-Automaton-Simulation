e4833d62a80b8c290771154e6fbdd5cc8f719d84
Lunairye <waowoaw@outlook.com>
2026-09-05 20:21:49 +0100

    Addition of UI classes

--- include/graphics.h
+++ include/graphics.h
@@ -4,6 +4,10 @@
 
 #include "grid.h"
 
+#include <memory>
+#include <string>
+#include <functional>
+
 struct UIStyle {
 
 };
@@ -18,6 +22,27 @@ public:
 	virtual void Update() {}
 	virtual void Draw(const UIStyle& style) = 0;
 	virtual ~UIElement() = default;
+};
+
+class Panel : public UIElement {
+private:
+
+public:
+	std::vector<std::unique_ptr<UIElement>> children;
+
+	void Update() override;
+	void Draw(const UIStyle& style) override;
+};
+
+class Button : public UIElement {
+private:
+
+public:
+	std::string label;
+	std::function<void()> onClick;
+
+	void Update() override;
+	void Draw(const UIStyle& style) override;
 };
 
 class Renderer {
